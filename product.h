923e04fe4542842dbd0e42b0e125f931dddc6bea
Goydaa <URNAZXC@GMAIL.COM>
2025-03-27 09:01:42 +0300

    Update product.h

--- product.h
+++ product.h
@@ -8,17 +8,16 @@ private:
     std::string name;
     double price;
     int quantity;
+    double minPrice; // Минимальная цена для продукта
 
 public:
-    Product(const std::string& name, double price, int quantity);
-
+    Product(const std::string& productName, double productPrice, int productQuantity, double productMinPrice = 0.0);
     std::string getName() const;
     double getPrice() const;
     int getQuantity() const;
-
-    void setPrice(double newPrice);
-    void setQuantity(int newQuantity);
+    double getMinPrice() const;
+    bool isAffordable(double money) const;
+    void reduceQuantity(int amount);
 };
 
-#endif // PRODUCT_H
-
+#endif

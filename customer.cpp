947a73ee62c229aead552ead85bfd862f009f12c
Goydaa <URNAZXC@GMAIL.COM>
2025-03-27 09:03:39 +0300

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -1,32 +1,30 @@
 #include "Customer.h"
 #include <iostream>
 
-Customer::Customer(const std::string& name, double balance)
-    : name(name), balance(balance) {
-}
+Customer::Customer(const std::string& customerName, double initialMoney)
+    : name(customerName), money(initialMoney) {}
 
 std::string Customer::getName() const {
     return name;
 }
 
-double Customer::getBalance() const {
-    return balance;
+double Customer::getMoney() const {
+    return money;
 }
 
 void Customer::purchase(Store& store, const std::string& productName, int quantity) {
-    double price = store.findPrice(productName);
-    if (price == -1) {
-        std::cout << "Товар не найден!" << std::endl;
-        return;
-    }
-
-    double totalCost = price * quantity;
-    if (balance >= totalCost && store.purchaseProduct(productName, quantity)) {
-        balance -= totalCost;
-        std::cout << name << " купил " << quantity << " шт. " << productName << " за " << totalCost << " руб.\n ";
-    }
-    else {
-        std::cout << " Недостаточно средств или товар в недоступном количестве " << std::endl;
+    Product* product = store.findProduct(productName);
+    if (product && product->getQuantity() >= quantity) {
+        double totalPrice = product->getPrice() * quantity;
+        if (product->isAffordable(money)) {
+            product->reduceQuantity(quantity);
+            money -= totalPrice;
+            std::cout << name << " купил " << quantity << " " << productName
+                      << "(ов) за " << totalPrice << " рублей. Остаток на счету: " << money << " рублей.\n";
+        } else {
+            std::cout << "Недостаточно денег для покупки " << quantity << " " << productName << "(ов).\n";
+        }
+    } else {
+        std::cout << "Товар не найден или недостаточно количества " << productName << ".\n";
     }
 }
-

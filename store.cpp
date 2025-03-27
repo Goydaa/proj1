8fd18f59630ce8dbaac68e5386fb54a5aeb7a90b
Goydaa <URNAZXC@GMAIL.COM>
2025-03-27 08:52:54 +0300

    Update store.cpp

--- store.cpp
+++ store.cpp
@@ -1,31 +1,17 @@
 #include "Store.h"
 
-Store::Store(const std::string& name) : name(name) {}
-
-std::string Store::getName() const {
-    return name;
-}
+Store::Store(std::string name) : name(name) {}
 
 void Store::addProduct(const Product& product) {
     products.push_back(product);
 }
 
-bool Store::purchaseProduct(const std::string& productName, int quantity) {
+Product* Store::findProduct(const std::string& productName) {
     for (auto& product : products) {
-        if (product.getName() == productName && product.getQuantity() >= quantity) {
-            product.setQuantity(product.getQuantity() - quantity);
-            return true;
-        }
-    }
-    return false;
-}
-
-double Store::findPrice(const std::string& productName) const {
-    for (const auto& product : products) {
         if (product.getName() == productName) {
-            return product.getPrice();
+            return &product;
         }
     }
-    return -1; 
+    return nullptr;
 }
 

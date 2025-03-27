ee261e860ee8dd8e28b2d4b7e609d12fd38d6947
Goydaa <URNAZXC@GMAIL.COM>
2025-03-27 09:04:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,14 @@
 #include <iostream>
 
 int main() {
-    // Создание магазина
     Store store("TechStore");
-    store.addProduct(Product("Laptop", 50000, 5, 50000));  
-    store.addProduct(Product("Smartphone", 30000, 10, 35000)); 
-    store.addProduct(Product("Tablet", 15000, 8, 15000));  
-    store.addProduct(Product("Bananas", 50, 100, 50));  
+    store.addProduct(Product("Laptop", 50000, 5, 1.0));
+    store.addProduct(Product("Smartphone", 30000, 10, 1.0));
 
-    // Создание клиента
     Customer customer("Ivan", 60000);
-
-    // Покупки
     customer.purchase(store, "Laptop", 1);
     customer.purchase(store, "Smartphone", 2);
-    customer.purchase(store, "Laptop", 5);  
-    customer.purchase(store, "Bananas", 30);  
+    customer.purchase(store, "Laptop", 5);
 
     return 0;
 }
